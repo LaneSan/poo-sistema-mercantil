19ba9d89c269efa071c9c06a8ad7353e889eeee0
Ale <alessandrokame@gmail.com>
2018-11-25 20:48:16 -0300

    l

--- Principal.ts
+++ Principal.ts
@@ -1,3 +1,8 @@
 declare function require(name: string): any;
 var input = require('readline-sync');
 
+while(true) {
+    let op : string = input.question("*** Bem Vindo ***\n 1- Logar\n 2- Cadastrar-se como cliente\n 3- Sair\n Digite uma opcao: ");
+	console.log("");
+} 
+
