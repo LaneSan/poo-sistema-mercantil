4ff75bfef2e6e1f6d6105e54437f9629b6c19fc3
Shelida Crislane <shelidacrislane@gmail.com>
2018-11-25 21:28:27 -0300

    att2

--- Principal.ts
+++ Principal.ts
@@ -1,8 +1,44 @@
 declare function require(name: string): any;
 var input = require('readline-sync');
 
+let cmd: string;
+
+let i: string = null;
+let s: string = null;
+
 while(true) {
-    let op : string = input.question("*** Bem Vindo ***\n 1- Logar\n 2- Cadastrar-se como cliente\n 3- Sair\n Digite uma opcao: ");
-	console.log("");
+    cmd = input.question("*** Bem Vindo ***\n 1- Logar\n 2- Cadastrar-se como cliente\n 3- Cadastrar-se como mercantil\n" +
+        "4- Sair\n Digite uma opcao: ");
+    if(cmd == "4"){
+        console.log("Tchau, volte sempre!");
+        break;
+    }
+
+    switch(cmd){
+        case "1":
+            i = input.question("Digite o id: ");
+            s = input.question("Digite a senha: ");
+            break;
+
+        case "2":
+            i = input.question("Digite o id: ");
+            s = input.question("Digite a senha: ");
+            if(){
+                
+            }
+            break;
+
+        case "3":
+            i = input.question("Digite o id: ");
+            s = input.question("Digite a senha: ");
+            if(){
+
+            }
+            break;
+
+        case "":
+
+    }
+        
 } 
 
